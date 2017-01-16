3a58d8d63f541a09baece2bc4daed26b581ed755
Michael Tharp <michael.tharp@sas.com>
2017-01-16 22:18:48 +0000

    Add jenkins build script
    Change-Id: I032c60d280af75806569eee1404c23d18cf52200

---
+++ jenkins.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+export WORKDIR=$(pwd)
+export GOPATH=$WORKDIR/go
+export GO15VENDOREXPERIMENT=1
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+rm -rf $WORKDIR/build
+
+go get -u -v github.com/Masterminds/glide
+
+module=$(cd checkout && glide name 2>/dev/null)
+version=$(cd checkout && git describe --tags |sed -e 's/-\([0-9]*\).*/.\1/')
+[ -n "$version" ] || { echo Unable to determine version; exit 1; }
+ldflags="-X ${module}.config.Version=$version"
+echo "Version: $version"
+mkdir -p $GOPATH/src/$(dirname $module)
+ln -sfn $WORKDIR/checkout $GOPATH/src/$module
+
+mkdir -p $WORKDIR/build
+cd $GOPATH/src/$module
+glide i
+touch $GOPATH/src/$module/config/config.go # Make sure version gets updated
+GOOS=linux go build -v -ldflags "$ldflags" -o $WORKDIR/build/relic $module/relic
+GOOS=windows go build -v -ldflags "$ldflags" -o $WORKDIR/build/relic.exe $module/relic/relic_notoken
+
+cd $WORKDIR/build
+mkdir dist-redhat dist-windows
+cp -a $WORKDIR/checkout/distro/linux/* relic dist-redhat/
+sed -i -e "s/^Version:/Version: $version/" dist-redhat/relic.spec
+tar -czf relic-src-redhat-${version}.tar.gz dist-redhat
+cp -a $WORKDIR/checkout/distro/windows/* relic.exe dist-windows/
+sed -i -e "s/ Version=[^ >]*/ Version='$version'/" dist-windows/relic.wxs
+zip -rq relic-src-windows-${version}.zip dist-windows
+rm -rf dist-*
