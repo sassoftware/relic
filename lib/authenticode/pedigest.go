b51fa13f6bef602a981f13ade000d40936b24068
Chris Roberts <croberts@hashicorp.com>
2023-04-11 16:24:37 -0700

    Adjust section size when not aligned
    In some cases the size of a section defined in the header will not
    include the padding added for page alignment. Check section sizes and if
    the section is not properly aligned, adjust the size so it is properly
    aligned.

--- lib/authenticode/pedigest.go
+++ lib/authenticode/pedigest.go
@@ -240,6 +240,7 @@ func readOptHeader(r io.Reader, d io.Writer, peStart int64, fh *pe.FileHeader) (
 		dd4Start = 128
 		hvals.sizeOfHdr = int64(opt.SizeOfHeaders)
 		hvals.sectionAlign = int(opt.SectionAlignment)
+		hvals.fileAlign = int(opt.FileAlignment)
 	case 0x20b:
 		// PE32+
 		var opt pe.OptionalHeader64
@@ -253,6 +254,7 @@ func readOptHeader(r io.Reader, d io.Writer, peStart int64, fh *pe.FileHeader) (
 		dd4Start = 144
 		hvals.sizeOfHdr = int64(opt.SizeOfHeaders)
 		hvals.sectionAlign = int(opt.SectionAlignment)
+		hvals.fileAlign = int(opt.FileAlignment)
 	default:
 		return nil, errors.New("unrecognized optional header magic")
 	}
@@ -293,6 +295,16 @@ func readSections(r io.Reader, d io.Writer, fh *pe.FileHeader, hvals *peHeaderVa
 		}
 		break
 	}
+	// Check for any sections that are not properly aligned
+	// and adjust alignment if required
+	for i := 0; i < len(sections); i++ {
+		if sections[i].SizeOfRawData == 0 {
+			continue
+		}
+		if v := sections[i].SizeOfRawData % uint32(hvals.fileAlign); v != 0 {
+			sections[i].SizeOfRawData += uint32(hvals.fileAlign) - v
+		}
+	}
 	// hash the padding after the section table
 	if _, err := io.CopyN(d, r, hvals.sizeOfHdr-secTblEnd); err != nil {
 		return nil, err
@@ -331,6 +343,8 @@ type peHeaderValues struct {
 	sizeOfHdr int64
 	// section alignment in memory
 	sectionAlign int
+	// section alignment in file
+	fileAlign int
 	// file offset and size of the certificate table
 	certStart, certSize int64
 }
