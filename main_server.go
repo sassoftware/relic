61c69be804b90734a8091f564d9ee5e26d7da608
hodbn <hodbn@users.noreply.github.com>
2024-09-01 20:46:57 +0300

    chore: go mod tidy

--- main_server.go
+++ main_server.go
@@ -21,8 +21,8 @@ package main
 
 // Server and server-adjacent commands
 import (
-	_ "github.com/sassoftware/relic/v8/cmdline/auditor"
-	_ "github.com/sassoftware/relic/v8/cmdline/notarycmd"
-	_ "github.com/sassoftware/relic/v8/cmdline/servecmd"
-	_ "github.com/sassoftware/relic/v8/cmdline/workercmd"
+	_ "github.com/mind-security/relic/v8/cmdline/auditor"
+	_ "github.com/mind-security/relic/v8/cmdline/notarycmd"
+	_ "github.com/mind-security/relic/v8/cmdline/servecmd"
+	_ "github.com/mind-security/relic/v8/cmdline/workercmd"
 )
